fdfa5ab2b4b8b013a4d77ebba44dffa4471612d1
sathyatg3377 <140390235+sathyatg3377@users.noreply.github.com>
2026-07-27 21:12:03 +0530

    Update fortify.sh

--- fortify.sh
+++ fortify.sh
@@ -1,21 +1,40 @@
 #!/bin/bash
 set -e
 
-echo "Installing Fortify CLI..."
+echo "===== Installing Fortify CLI ====="
 
-curl -L https://github.com/fortify/fcli/releases/latest/download/fcli-linux.tgz -o fcli.tgz
+curl -L \
+https://github.com/fortify/fcli/releases/latest/download/fcli-linux.tgz \
+-o fcli.tgz
 
 mkdir -p fcli
 tar -xzf fcli.tgz -C fcli
-
 chmod +x fcli/fcli
 
 export PATH=$PWD/fcli:$PATH
 
-echo "Fortify CLI Version:"
+echo "Fortify CLI Version"
 fcli --version
 
-echo "Logging in..."
+echo "===== Downloading ScanCentral Client ====="
+
+curl -L \
+-u "$FOD_USER:$FOD_PASSWORD" \
+https://ams.fortify.com/Tools/ScanCentral/Fortify_ScanCentral_Client_Latest_x64.zip \
+-o Fortify_ScanCentral_Client.zip
+
+unzip -q Fortify_ScanCentral_Client.zip -d scancentral
+
+chmod +x scancentral/scancentral
+chmod +x scancentral/packagescanner
+chmod +x scancentral/pwtool
+
+export PATH=$PWD/scancentral:$PATH
+
+echo "ScanCentral Version"
+scancentral --version
+
+echo "===== Login to FoD ====="
 
 fcli fod session login \
   --url "$FOD_URL" \
@@ -23,28 +42,28 @@ fcli fod session login \
   -p "$FOD_PASSWORD" \
   -t "$FOD_TENANT"
 
-echo "Installing Node.js dependencies..."
+echo "===== Node Build ====="
 
 npm install
 
-echo "Packaging Source..."
+echo "===== Package ====="
 
 scancentral package -o package.zip
 
-echo "Starting Scan..."
+echo "===== Start Scan ====="
 
 fcli fod sast-scan start \
   --release "$FOD_RELEASE" \
   --file package.zip
 
-echo "Waiting for Scan..."
+echo "===== Wait ====="
 
 fcli fod sast-scan wait
 
-echo "Checking Policy..."
+echo "===== Policy Check ====="
 
 fcli fod action run check-policy
 
-echo "Logout..."
+echo "===== Logout ====="
 
 fcli fod session logout
